bc3a6c22bc6c0781138d7ccca96c65a57b1bb471
alfanse <alex.fanshawe@gmail.com>
2016-07-12 23:36:25 +0100

    AF renames class to class+Builder

--- src/app/builder.js
+++ src/app/builder.js
@@ -5,6 +5,30 @@ function doSubmitSource() {
   document.getElementById("output-div").style.display="block";
 }
 
-exports.convertBeanToBuilder = function(source) {
-  return source;
+/** Converts the source
+(expected to be a java bean with a constructor with params)
+ to a builder class */
+exports.convertUsingConstructor = function(source) {
+  var sourceArray = source.split('\n');
+  var output = "";
+  var classNameFinder = / class\s*(\w*)\s*[{|extends|implements|\n]/;
+  var classname;
+
+  for(var rowIndex = 0; rowIndex < sourceArray.length; ++rowIndex){
+      //reintroduce the line break
+      if(rowIndex>0) {
+        output+="\n";
+      }
+      var parsedRow = sourceArray[rowIndex];
+
+      //find class declaration
+      if(~parsedRow.indexOf(' class ')){
+         var regGroup = classNameFinder.exec(parsedRow);
+         classname = regGroup[1];
+         parsedRow = parsedRow.replace(" "+classname+" ", " "+classname+"Builder ");
+      }
+
+      output+=parsedRow;
+  }
+  return output;
 };
--- src/test/builderTest.js
+++ src/test/builderTest.js
@@ -1,8 +1,30 @@
 var assert = require('chai').assert;
-var convertBeanToBuilder = require('../app/builder.js').convertBeanToBuilder;
+var builder = require('../app/builder.js');
 
-describe('convertBeanToBuilder', function() {
-  it('should return whats given', function() {
-    assert.equal(convertBeanToBuilder('given stuff'), "given stuff");
+describe('convertUsingConstructor', function() {
+
+  it('should output package, imports and javadoc unchanged', function() {
+    var input = "package one;\n"+"import two.three.Thing;"+"/** some java doc */";
+    var output = builder.convertUsingConstructor(input);
+    assert.equal(output, input);
+  }),
+
+  it('should rename class with name+Builder', function() {
+    var input ="public class Alpha {\n" + "\n" +"}";
+    var expected ="public class AlphaBuilder {\n" + "\n" +"}"
+    assert.equal(builder.convertUsingConstructor(input), expected);
   });
-});
+
+  it('should rename class with name+Builder when extends other class', function() {
+    var input ="public class Alpha extends Object\n" + "{\n" +"}";
+    var expected ="public class AlphaBuilder extends Object\n" + "{\n" +"}"
+    assert.equal(builder.convertUsingConstructor(input), expected);
+  });
+
+
+  it('should rename class with name+Builder when implements an interface', function() {
+    var input ="public class Alpha implements Thing {\n" + "\n" +"}";
+    var expected ="public class AlphaBuilder implements Thing {\n" + "\n" +"}"
+    assert.equal(builder.convertUsingConstructor(input), expected);
+  });
+})
